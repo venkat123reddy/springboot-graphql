c8dfbad1a212f8b641ef7d698520a651390f44d5
ATTALA VENKATA REDDY <36073543+venkat123reddy@users.noreply.github.com>
2024-09-09 00:14:59 -0500

    'intial-commit'

---
+++ src/main/java/com/farmfresh/farmfresh/FarmfreshUcmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.farmfresh.farmfresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmfreshUcmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmfreshUcmProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmfresh/farmfresh/FarmfreshUcmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmfresh.farmfresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmfreshUcmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
